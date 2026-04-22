dbf5d1c98e60e63ca764121d87e53e7d269e1c13
corleonev975-crypto <corleonev975@gmail.com>
2026-04-22 07:32:46 +0700

    Delete css/user.js

--- css/user.js
+++
@@ -1,8 +0,0 @@
-const user = JSON.parse(localStorage.getItem("user"));
-
-if (user) {
-  const btn = document.querySelector(".login-btn");
-  if (btn) {
-    btn.innerText = user.displayName;
-  }
-}
