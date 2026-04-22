31b2abf4f8b8c97215eb3cf99de4cd0dc4ff0b60
corleonev975-crypto <corleonev975@gmail.com>
2026-04-22 08:56:02 +0700

    Update Firebase config with user-specific details
    Updated Firebase configuration with user-specific details and added initialization for Firebase services.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,26 @@
-// Firebase Config
-export const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_AUTH_DOMAIN",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_STORAGE",
-  messagingSenderId: "ISI_SENDER_ID",
-  appId: "ISI_APP_ID"
+// FIREBASE CONFIG
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+// CONFIG DARI FIREBASE KAMU
+const firebaseConfig = {
+  apiKey: "ISI_APIKEY_KAMU",
+  authDomain: "ISI_AUTH_DOMAIN_KAMU",
+  projectId: "ISI_PROJECT_ID_KAMU",
+  storageBucket: "ISI_STORAGE_KAMU",
+  messagingSenderId: "ISI_SENDER_ID_KAMU",
+  appId: "ISI_APP_ID_KAMU"
 };
+
+
+// INIT
+const app = initializeApp(firebaseConfig);
+
+// EXPORT BIAR BISA DIPAKAI FILE LAIN
+export const auth = getAuth(app);
+export const db = getFirestore(app);
