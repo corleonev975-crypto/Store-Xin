2ef002d7e7a451a357901d2f892c0ff98039efa7
corleonev975-crypto <corleonev975@gmail.com>
2026-04-22 07:33:04 +0700

    Delete css/firebase-conpig.js

--- css/firebase-conpig.js
+++
@@ -1,9 +0,0 @@
-// Firebase Config
-export const firebaseConfig = {
-  apiKey: "ISI_API_KEY",
-  authDomain: "ISI_AUTH_DOMAIN",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_STORAGE",
-  messagingSenderId: "ISI_SENDER_ID",
-  appId: "ISI_APP_ID"
-};
